8caaa2b37fd5670529d6891b6f1beb0104a97ff5
Muhammad Faseeh Ud Din <mfaseehuddin2001@gmail.com>
2022-09-13 09:38:24 +0500

    Updated:     - the recursive factorial function

--- Lecture 3/Recurrsion-Iteration.cpp
+++ Lecture 3/Recurrsion-Iteration.cpp
@@ -4,64 +4,63 @@ using namespace std;
 // function prototypes
 void reverseIntegerIteration(int inputNum);
 void reverseIntegerRecursive(int currNum);
-int FactorialRecursion(int inputNum, int total);
+int FactorialRecursion(int inputNum);
 
 int main()
 {
     reverseIntegerIteration(123456789);
     reverseIntegerRecursive(123456789);
     cout << endl;
-    FactorialRecursion(5,1);
+    cout << "factorial: " << FactorialRecursion(5) << endl;
     return 0;
 }
 
 // example
 // input an integer and print reversed
-// iteration -- Anything involving repitition of steps
-void reverseIntegerIteration(int inputNum)
-{
-    int currNum = inputNum;
-    while (currNum > 0)
+    // iteration -- Anything involving repitition of steps
+    void reverseIntegerIteration(int inputNum)
     {
-        cout << currNum % 10;
-        currNum /= 10;
+        int currNum = inputNum;
+        while (currNum > 0)
+        {
+            cout << currNum % 10;
+            currNum /= 10;
+        }
+        cout << endl;
     }
-    cout << endl;
-}
 
-// recursion -- Calling a function within itself
-void reverseIntegerRecursive(int currNum)
+    // recursion -- Calling a function within itself
+    void reverseIntegerRecursive(int currNum)
+    {
+        if (currNum == 0)
+            return;
+        cout << currNum % 10;
+        reverseIntegerRecursive(currNum / 10);
+    }
+
+        //call stack for the reverseIntegerRecursive function
+        //|---------------|
+        //|r = 0          |
+        //|currNum = 0    |reverseIntegerRecursive(0)
+        //|---------------|
+        //|r = 1          |
+        //|currNum = 1    |reverseIntegerRecursive(1)
+        //|---------------|
+        //|r = 3          |
+        //|currNum = 13   |reverseIntegerRecursive(13)
+        //|---------------|
+        //|r = 5          |
+        //|currNum = 135  |reverseIntegerRecursive(135)
+        //|---------------|
+        //|r = 7          |
+        //|currNum = 1357 |reverseIntegerRecursive(1357)
+        //|---------------|
+        //|               |main()
+        //|---------------|
+
+int FactorialRecursion(int inputNum)
 {
-    if (currNum == 0)
-        return;
-    cout << currNum % 10;
-    reverseIntegerRecursive(currNum / 10);
-}
-
-// call stack
-//|---------------|
-//|r = 0          |
-//|currNum = 0    |reverseIntegerRecursive(0)
-//|---------------|
-//|r = 1          |
-//|currNum = 1    |reverseIntegerRecursive(1)
-//|---------------|
-//|r = 3          |
-//|currNum = 13   |reverseIntegerRecursive(13)
-//|---------------|
-//|r = 5          |
-//|currNum = 135  |reverseIntegerRecursive(135)
-//|---------------|
-//|r = 7          |
-//|currNum = 1357 |reverseIntegerRecursive(1357)
-//|---------------|
-//|               |main()
-//|---------------|
-
-int FactorialRecursion(int inputNum,int total)
-{
-    if(inputNum == 1){return 1;}
-    cout << total << endl;
-    total = inputNum * FactorialRecursion(inputNum-1, total);
-    
+    if (inputNum == 1)
+        return 1;
+    return inputNum * FactorialRecursion(inputNum - 1);
 }
