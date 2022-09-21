2e996fc053b724845e726b6227dab8ea06036078
Muhammad Faseeh Ud Din <mfaseehuddin2001@gmail.com>
2022-09-21 13:49:29 +0500

    - Updated:      - Completed Lecture 6 -Classes.cpp

--- Week 3/lecture 6/classes.cpp
+++ Week 3/lecture 6/classes.cpp
@@ -10,10 +10,21 @@ class Date {
     int year;
     //public members
     public:
-        //constructor
+        //default constructor
+        Date();
+        //parameterized constructor
         Date(int d, int m, int y);
+        //parameterized constructor overloading
+        Date(int d, int m);
+        Date(int d);
         //member functions
         void print();
+        //setter functions
+        void setDate(int d, int m, int y);
+
+        //destructor
+        ~Date();
+        
 
         //basic operator overloading
         //not taught in lecture
@@ -26,8 +37,34 @@ Date::Date(int d, int m, int y) {
     month = m;
     year = y;
 }
+Date::Date(int d, int m) {
+    day = d;
+    month = m;
+    year = 2019;
+}
+Date::Date(int d) {
+    day = d;
+    month = 1;
+    year = 2019;
+}
+Date::Date() {
+    day = 1;
+    month = 1;
+    year = 2019;
+}
+void Date::setDate(int d, int m, int y) {
+    day = d;
+    month = m;
+    year = y;
+}
 void Date::print() {
     cout << day << "/" << month << "/" << year << endl;
+}
+Date::~Date() {
+    //here we can delete any dynamically allocated memory
+    //in this case we don't have any
+    //hence, we just print a message
+    cout << "destructor called" << endl;
 }
 
 
@@ -56,6 +93,13 @@ int main()
     //d1 is passed to print() implicitly
     //d1.day is a private member, so it cannot be accessed outside the class definition!
     //cout << d1.day << endl; will result in an error - specifically, a compiler error
+    //to access private members, we need to use public member functions, called getters and setters.
+
+    Date d4; //in this case the default constructor is called, since no arguments are passed. The constructor to be called is determined by the arguments passed.
+    d4.print();
+    d4.setDate(21, 9, 2022);//calling the setter function
+    d4.print();
+
 
     //Declare d2 as instance/object of class Date
     //adding 10 days to d1: not taught in lecture
