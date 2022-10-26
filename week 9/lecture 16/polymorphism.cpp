1607e38e79ec725fbcdfb5eb3fc490f2d28d0ac4
Muhammad Faseeh Ud Din <mfaseehuddin2001@gmail.com>
2022-10-26 13:56:32 +0500

    -Update: New Stuff

--- week 9/lecture 16/polymorphism.cpp
+++ week 9/lecture 16/polymorphism.cpp
@@ -12,7 +12,7 @@ public:
     {
         cout << "print(): Base" << endl;
     }
-    ~Base()
+    virtual ~Base()
     {
         cout << "Destructor: Base" << endl;
     }
@@ -43,47 +43,54 @@ public:
 /////////but i made the derived class print function a virtual function and it still works, why? -> because the base class print function is virtual
 /////////but it was not virtual in the base class, why does it work? -> because the base class print function is virtual
 
-// int main(){
-//     Base *b = new Base();
-//     b->print();
-//     b = new Derived();
-//     b->print(); 
-//     return 0;
-// }
-
-int main()
-{
-
-    //when the base class print function was not virtual, this was static binding/compile time binding/early binding
-    //when the base class print function was virtual, this was dynamic binding/run time binding/late binding
-
-    //explain the difference between static and dynamic binding
-    //static binding is when the compiler knows which function to call at compile time
-    //dynamic binding is when the compiler does not know which function to call at compile time, so it waits until run time to figure out which function to call
-        //this is done by using a virtual function table
-            //what is that?
-                //a virtual function table is a table that contains the addresses of all the virtual functions in the class
-                //the virtual function table is stored in the class object
-                //the virtual function table is used to determine which function to call at run time
-                //the virtual function table is created when the class object is created
-
-
-
-    cout << "step1:" << endl;
-    Base *bPtr = new Base();
-    bPtr->print();
+int main(){
+    Base *b = new Base();
+    b->print();
+    delete b;
     cout << endl;
 
-    cout << "step2:" << endl;
-    Derived *dPtr = new Derived();
-    dPtr->print(); //what is happening here? -> the derived class print function is being called
+    b = new Derived();
+    b->print(); 
+    delete b; //this has a problem -> the base class destructor is called, but the derived class destructor is not called
+    //to fix this problem, we need to make the base class destructor virtual // as done in the class above
     cout << endl;
-
-    cout << "step3:" << endl;
-    bPtr = dPtr;
-    bPtr->print();
-    cout << endl;
-
-
     return 0;
 }
+
+// int main()
+// {
+
+//     //when the base class print function was not virtual, this was static binding/compile time binding/early binding
+//     //when the base class print function was virtual, this was dynamic binding/run time binding/late binding
+
+//     //explain the difference between static and dynamic binding
+//     //static binding is when the compiler knows which function to call at compile time
+//     //dynamic binding is when the compiler does not know which function to call at compile time, so it waits until run time to figure out which function to call
+//         //this is done by using a virtual function table
+//             //what is that?
+//                 //a virtual function table is a table that contains the addresses of all the virtual functions in the class
+//                 //the virtual function table is stored in the class object
+//                 //the virtual function table is used to determine which function to call at run time
+//                 //the virtual function table is created when the class object is created
+
+
+
+//     cout << "step1:" << endl;
+//     Base *bPtr = new Base();
+//     bPtr->print();
+//     // delete bPtr;
+//     cout << endl;
+    
+//     cout << "step2:" << endl;
+//     Derived *dPtr = new Derived();
+//     dPtr->print(); //what is happening here? -> the derived class print function is being called
+//     cout << endl;
+
+//     cout << "step3:" << endl;
+//     bPtr = dPtr;
+//     bPtr->print();
+//     cout << endl;
+
+
+//     return 0;
+// }
